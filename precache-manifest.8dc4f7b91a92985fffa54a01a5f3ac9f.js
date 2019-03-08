c03d10776071ad40d49bb94f11f04590c757f56a
twistezo <kolkolukasz@gmail.com>
2019-03-08 14:42:45 +0100

    Updates

---
+++ precache-manifest.8dc4f7b91a92985fffa54a01a5f3ac9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29f8c1e977d2fa351e2f",
+    "url": "/job-board/static/js/runtime~main.29f8c1e9.js"
+  },
+  {
+    "revision": "63b55021b1109efd12b9",
+    "url": "/job-board/static/js/main.63b55021.chunk.js"
+  },
+  {
+    "revision": "5e9b3af471ca610bb10f",
+    "url": "/job-board/static/js/2.5e9b3af4.chunk.js"
+  },
+  {
+    "revision": "63b55021b1109efd12b9",
+    "url": "/job-board/static/css/main.a214dd84.chunk.css"
+  },
+  {
+    "revision": "5e9b3af471ca610bb10f",
+    "url": "/job-board/static/css/2.48a8e970.chunk.css"
+  },
+  {
+    "revision": "82fe4d1e01c0b1775eb14ac0b98db993",
+    "url": "/job-board/index.html"
+  }
+];
